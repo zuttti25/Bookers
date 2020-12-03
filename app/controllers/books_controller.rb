f4c7b4085e64f761441ae59c6a7c662984704f4c
zuttti25 <xxx.pxjpp@icloud.com>
2020-12-03 03:47:15 +0000

    Bookers 制作途中

---
+++ app/controllers/books_controller.rb
@@ -0,0 +1,40 @@
+class BooksController < ApplicationController
+  def index
+  end
+
+  def show
+    @book = Book.find(params[:id])
+  end
+
+  def new
+    @book = Book.new
+    @books = Book.all
+  end
+  
+  def create
+    book = Book.new(book_params)
+    book.save
+    redirect_to book_path(book.id)
+  end
+
+  def edit
+    @book = Book.find(params[:id])
+  end
+  
+  def update
+    book = Book.find(params[:id])
+    book.update(book_params)
+    redirect_to book_path(book.id)
+  end
+    
+  def destroy
+    book = Book.find(params[:id])
+    book.destroy
+    redirect_to books_path
+  end
+  
+  private
+  def book_params
+    params.require(:book).permit(:title, :body)
+  end
+end
