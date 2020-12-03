3be27f99d73ea45df8b1fd976ab9d1a968ef4d1f
zuttti25 <xxx.pxjpp@icloud.com>
2020-12-03 12:30:03 +0000

    あとはCSSを整えるだけ！

--- app/controllers/books_controller.rb
+++ app/controllers/books_controller.rb
@@ -15,7 +15,7 @@ class BooksController < ApplicationController
     @book = Book.new(book_params)
     @books = Book.all
     if @book.save
-    redirect_to new_book_path(book.id)
+    redirect_to book_path(@book.id), notice: 'Book was successfully destroyed.'
     else 
     render action: :new
     end
@@ -29,7 +29,7 @@ class BooksController < ApplicationController
   def update
     @book = Book.find(params[:id])
     if @book.update(book_params)
-    redirect_to book_path(@book.id)
+    redirect_to book_path(@book.id), notice: 'Book was successfully destroyed.'
     else
      render action: :edit
     end
@@ -38,7 +38,7 @@ class BooksController < ApplicationController
   def destroy
     book = Book.find(params[:id])
     book.destroy
-    redirect_to new_book_path
+    redirect_to new_book_path, notice: 'Book was successfully destroyed.'
   end
   
   private
