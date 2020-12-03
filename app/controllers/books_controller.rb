d8d33bf94e084128311d59efe10ce1db599717b6
zuttti25 <xxx.pxjpp@icloud.com>
2020-12-03 10:55:36 +0000

    editのrenderの修復完了

--- app/controllers/books_controller.rb
+++ app/controllers/books_controller.rb
@@ -23,14 +23,15 @@ class BooksController < ApplicationController
 
   def edit
     @book = Book.find(params[:id])
+    @books = Book.find(params[:id])
   end
   
   def update
-    book = Book.find(params[:id])
-    if book.update(book_params)
-    redirect_to book_path(book.id)
+    @book = Book.find(params[:id])
+    if @book.update(book_params)
+    redirect_to book_path(@book.id)
     else
-     render action: :edit 
+     render action: :edit
     end
   end
     
