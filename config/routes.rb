be42fd589d050b0c24af01e805b6d4c577e1c7fe
zuttti25 <xxx.pxjpp@icloud.com>
2020-12-08 08:56:21 +0000

    ログイン機能の実装

--- config/routes.rb
+++ config/routes.rb
@@ -1,6 +1,7 @@
 Rails.application.routes.draw do
+devise_for :users
+root to: 'books#index'
 get 'books' => 'books#new', as: 'new_book'
 resources :books
- root to: 'books#index'
-  # For details on the DSL available within this file, see http://guides.rubyonrails.org/routing.html
+resources :users, only: [:show]
 end
